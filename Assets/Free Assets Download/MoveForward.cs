be4f4ae32bc207992f888e440c65bdedcc532124
Johndale Alfred Julian <j.julian@klab.com>
2014-12-09 20:22:16 +0800

    Merge latest project folder PART 5

---
+++ Assets/Free Assets Download/MoveForward.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveForward : MonoBehaviour {
+
+    public float speed = 2.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //transform.position += transform.forward / 5.0f;
+        //transform.Translate(transform.forward);
+	}
+}
